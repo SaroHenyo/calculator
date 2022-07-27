774e5ce1f802ab2806a1713eb3929b783f62f162
SaroHenyo <cryptohenyo@gmail.com>
2022-07-27 18:52:22 +0800

    Calculator

---
+++ script.js
@@ -0,0 +1,52 @@
+let firstNumber;
+let secondNumber;
+let operation;
+let sum;
+
+console.log(firstNumber, operation, secondNumber);
+
+function hello() {
+    console.log("Hello, World!");
+}
+// Console Addition
+function clickNumber(input) {
+    if (operation === undefined) {
+        if (firstNumber === undefined) {
+            firstNumber = String(input);
+        } else {
+            firstNumber = String(firstNumber) + String(input);
+        }
+    } else {
+        if (secondNumber === undefined) {
+            secondNumber = String(input);
+        } else {
+            secondNumber = String(secondNumber) + String(input);
+        }
+    }
+
+    console.log(firstNumber, operation, secondNumber);
+}
+
+
+function clickOperate(input) {
+    switch (input) {
+        case '+': {
+            operation = '+'
+            break;
+        }
+        case '=': {
+            sum = Number(firstNumber)  + Number(secondNumber);
+            console.log(sum);
+            firstNumber = undefined;
+            secondNumber = undefined;
+            operation = undefined;
+            break;
+        }
+
+        default: {
+            console.log("Unknown operation: " + input);
+        }
+    }
+    
+    console.log(firstNumber, operation, secondNumber);
+}
